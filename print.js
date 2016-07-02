ef2bf0277c2deec80fb92dc37cc56b1798e71c95
JiasHuang <jias.tw@gmail.com>
2016-07-02 08:54:50 +0800

    modified:   print.js

--- print.js
+++ print.js
@@ -2,40 +2,50 @@
 var defw = 32;
 
 function Right(str, width) {
-	var line = str;
-	var cnt = width - str.length;
-	for (var i=0; i<cnt; i++)
-		line = ' ' + line;
-	return line;
+    var line = str;
+    var cnt = width - str.length;
+    for (var i=0; i<cnt; i++)
+        line = ' ' + line;
+    return line;
 }
 
 function Left(str, width) {
-	var line = str;
-	var cnt = width - str.length;
-	for (var i=0; i<cnt; i++)
-		line = line + ' ';
-	return line;
+    var line = str;
+    var cnt = width - str.length;
+    for (var i=0; i<cnt; i++)
+        line = line + ' ';
+    return line;
 }
 
 function Center(str, width) {
-	var line = str;
-	var cnt = (width - str.length) / 2;
-	for (var i=0; i<cnt; i++)
-		line = ' ' + line;
-	for (var i=0; i<cnt; i++)
-		line = line + ' ';
-	return line;
+    var line = str;
+    var cnt = (width - str.length) >> 1;
+    for (var i=0; i<cnt; i++)
+        line = ' ' + line;
+    for (var i=0; i<cnt; i++)
+        line = line + ' ';
+    return line;
 }
 
 function LineEnd() {
-	return '$intro$';
+    return '$intro$';
 }
 
 function LineCut() {
     var line = '';
-	for (var i=0; i<defw; i++)
-		line = line + '-';
+    for (var i=0; i<defw; i++)
+        line = line + '-';
     return line+'$intro$';
+}
+
+function FontSize() {
+    return '$small$';
+}
+
+function AlignDetail(name, price, qty, amount, width) {
+    if (name.length <= 16 && price.length <= 4 && qty.length <=4 && amount.length <= 5)
+        return Left(name, 16) + '$' + Right(price, 4) + 'x' + Right(qty, 4) + '=' + Right(amount, 5) + LineEnd(); 
+    return name + Right(' $ ' + price + ' x ' + qty + ' = ' + amount, defw-name.length) + LineEnd();
 }
 
 function ShowPrint_ProcObj(obj) {
@@ -45,9 +55,9 @@ function ShowPrint_ProcObj(obj) {
         return;
     }
 
-	var myName = localStorage.getItem(coName);
-	var myPhone = localStorage.getItem(coPhone);
-	var myAddr = localStorage.getItem(coAddr);
+    var myName = localStorage.getItem(coName);
+    var myPhone = localStorage.getItem(coPhone);
+    var myAddr = localStorage.getItem(coAddr);
 
     var text = '';
 
@@ -95,38 +105,43 @@ function ShowPrint_ProcObj(obj) {
 
     text += '<br>';
     text += '<div class=total>';
-    text += ' Item: '+ obj['count'] +' Total: '+ obj['total'];
+    text += ' Item: ' + obj['count'] + ' Total: ' + obj['total'];
     text += '</div>'
 
-	data = '$small$';
-	data += Center(myName, defw) + LineEnd();
-	data += Center('TEL: '+myPhone, defw) + LineEnd();
-	data += Center('ADDR: '+myAddr, defw) + LineEnd();
-	data += LineEnd();
+    data = '';
+    data += FontSize();
+    data += LineEnd();
+    data += LineEnd();
+    data += Center(myName, defw) + LineEnd();
+    data += Center('TEL: '+myPhone, defw) + LineEnd();
+    data += Center('ADDR: '+myAddr, defw) + LineEnd();
+    data += LineEnd();
 
-	data += Left('ID: '+obj['id']) + LineEnd();
-	data += Left('Customer: '+obj['customer']) + LineEnd();
-	data += Left('Date: '+date2text(obj['date'])) + LineEnd();
-	data += LineEnd();
+    data += Left('ID: '+obj['id']) + LineEnd();
+    data += Left('Customer: '+obj['customer']) + LineEnd();
+    data += Left('Date: '+date2text(obj['date'])) + LineEnd();
+    data += LineEnd();
 
     data += 'Detail:' + LineEnd();
     data += LineCut();
-	for (key in order) {
-		var name = key;
+    for (key in order) {
+        var name = key;
         var value = order[key].split(separator);
-        var price = value[0];
-        var qty = value[1];
+        var price = parseFloat(value[0]);
+        var qty = parseFloat(value[1]);
         var amount = float_m(price, qty);
-		data += name + Right('$' + price + ' x ' + qty + ' = $' + amount, defw-name.length) + LineEnd();
-	}
+        data += AlignDetail(name, price.toFixed(1).toString(), qty.toFixed(1).toString(), amount.toFixed(1).toString(), defw);
+    }
     data += LineCut();
 
-	data += LineEnd();
-	data += Right('Total Items: '+ obj['count'], defw) + LineEnd();
-	data += Right('Total Amount: $'+ obj['total'], defw) + LineEnd();
-    data += LineCut();
+    data += Right(' Item: ' + obj['count'] + ' Total: $' + obj['total'], defw) + LineEnd();
+    data += LineEnd();
+    data += LineEnd();
+    data += LineEnd();
+    data += LineEnd();
+    data += LineEnd();
 
-	text += '<hr><a href="com.fidelier.printfromweb://'+data+'">Android POS Print Driver</a><hr>';
+    text += '<hr><a href="com.fidelier.printfromweb://'+data+'">Android POS Print Driver</a><hr>';
 
     document.getElementById('PrintResult').innerHTML=text;
 }
